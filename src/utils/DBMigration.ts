2330ea1ba33b5ad895726a92c6c38a6f3b116866
arommelaere <1508444+arommelaere@users.noreply.github.com>
2025-09-16 17:55:19 +0700

    Fixed clerk usage

--- src/utils/DBMigration.ts
+++ src/utils/DBMigration.ts
@@ -1,14 +1,14 @@
-import path from 'node:path';
-import { migrate } from 'drizzle-orm/node-postgres/migrator';
-import { createDbConnection } from './DBConnection';
+// import path from 'node:path';
+// import { migrate } from 'drizzle-orm/node-postgres/migrator';
+// import { createDbConnection } from './DBConnection';
 
-// Create a new and dedicated database connection for running migrations
-const db = createDbConnection();
+// // Create a new and dedicated database connection for running migrations
+// const db = createDbConnection();
 
-try {
-  await migrate(db, {
-    migrationsFolder: path.join(process.cwd(), 'migrations'),
-  });
-} finally {
-  await db.$client.end();
-}
+// try {
+//   await migrate(db, {
+//     migrationsFolder: path.join(process.cwd(), 'migrations'),
+//   });
+// } finally {
+//   await db.$client.end();
+// }
